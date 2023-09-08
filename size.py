428a2cf26c4778bbfc5afd77d0214660262ff8cc
tylermeyers <88351548+tylermeyers@users.noreply.github.com>
2023-09-08 14:13:41 -0700

    Added options, need to add unit check

--- size.py
+++ size.py
@@ -1,5 +1,4 @@
 from collections import deque
-import math
 #recipe sizes converter
 
 # 2 options :(1) Based off of entired recipe (ie only want half the portions)
@@ -9,7 +8,6 @@ class Ingredient:
         def __init__(self, in_size) -> None:
             num = ''
             unit = ''
-
             for i in in_size:
                 if(i.isdigit()):
                     num = num+i
@@ -30,14 +28,21 @@ class Ingredient:
         print(self.name +": " +self.size.get_parse())
 
 
-
-#ingredient1.in_print()
-
-#Initializing important var's
 q = deque()
-num_ingre = int(input("How many ingredients are there in your recipe? ").strip())
-conversion_size = float(input("By how much do you want convert the recipe(ie .5, 1.5): "))
 
+if(int(input("Option: 0 (conversion based off ingredient) or option 1 (conversion based off entire recipe?)"))):
+    conversion_size = float(input("By how much do you want convert the recipe(ie .5, 1.5): "))
+else:
+    ingredient_name = input("Ingredient name: ").strip()
+    amount = input("Amount you have: ").strip()
+    ingredient = Ingredient(ingredient_name, amount )
+
+    conversion_size = ingredient.size.num/Ingredient.parsed_size(input("Amount in recipe: ").strip()).num
+    q.append(ingredient)
+    print("DONT RE-ENTER INGREDIENT AND DO NOT INCLUDE IN NEXT QUESTION")
+
+
+num_ingre = int(input("How many ingredients are there in your recipe? ").strip())
 
 
 #Collecting all ingredients + the size
@@ -46,10 +51,6 @@ for i in range(num_ingre):
     ingredient.size.num = ingredient.size.num*conversion_size
     q.append(ingredient)
 
+print("Ingredients: ")
 while(bool(q)):
     Ingredient.in_print(q.popleft())
-
-
-
-
-
